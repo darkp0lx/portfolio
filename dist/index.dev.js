a8230ed1c950e338e857207ad7df5019f13212b8
hector <polbarazorda@gmail.com>
2021-01-21 22:01:41 -0500

    arreglando name y envio de correo

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -8,15 +8,21 @@ var data = [{
   code: "https://github.com/darkp0lx/pepsiSlider",
   web: "https://darkp0lx.github.io/pepsiSlider/"
 }, {
-  title: "Baamago Tatto",
-  text: "Aplicacion que carga solo una vez",
+  title: "facebook clone",
+  text: "Reecree un clon de facebook!! esta echo en React y tiene por backend firebase.En esta App se puede postear fotos y tambien comentarlas, inicia sesión con tu cuenta de gmail y pruebala :3",
   tec: ["svelte", "css", "html", "javascript"],
-  code: "https://github.com/darkp0lx/baamago",
-  web: "https://5f89c28cba3dad00070cd9ab--baamagotatto.netlify.app/"
+  code: "https://github.com/darkp0lx/facebook-clone",
+  web: "https://facebook-clone-416f5.web.app"
 }, {
-  title: "Portal de peliculas",
-  text: "Aplicacion creada en react para la consulta de informacion de todas las peliculas usando una API",
+  title: "Portal de películas",
+  text: "Aplicación creada en react para la consulta de información de todas las películas usando una API",
   tecnologias: [],
+  code: "https://github.com/darkp0lx/films2",
+  web: "https://films0.herokuapp.com/"
+}, {
+  title: "Bammaago Tatto",
+  text: "Aplicación SPA(single page application) aplicación que carga solo una vez",
+  tecnologías: [],
   code: "https://github.com/darkp0lx/films2",
   web: "https://films0.herokuapp.com/"
 }];
@@ -89,4 +95,45 @@ function setTheme(mode) {
   }
 
   localStorage.setItem("theme", mode);
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  // get the form elements defined in your form HTML above
+  var form = document.getElementById("contact-form");
+  var status = document.getElementById("status"); // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    status.innerHTML = "Correo recibido! Gracias";
+  }
+
+  function error() {
+    status.innerHTML = "Oops! no pudimos enviar tu correo";
+  } // handle the form submission event
+
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    status.classList.add("activo");
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+}); // helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+
+  xhr.send(data);
 }
